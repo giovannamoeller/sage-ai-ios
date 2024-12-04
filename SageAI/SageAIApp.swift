6c8457653266cdebb444ddc1f5c12d4e33ed57d1
giovannamoeller <giovannagmoeller@gmail.com>
2024-12-04 20:05:35 -0300

    Initial Commit

---
+++ SageAI/SageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SageAIApp.swift
+//  SageAI
+//
+//  Created by Giovanna Moeller on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SageAITests/SageAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SageAITests.swift
+//  SageAITests
+//
+//  Created by Giovanna Moeller on 04/12/24.
+//
+
+import Testing
+@testable import SageAI
+
+struct SageAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
